c690d6db3bdd5e2fb650d12cf3509a00550ddfd3
hui xu <hui.xu@spglobal.com>
2022-04-02 15:56:46 +0800

    hash table 应用

---
+++ src/Chapter3/Section5/DeDup.java
@@ -0,0 +1,20 @@
+package Chapter3.Section5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.HashSet;
+
+public class DeDup {
+    public static void main(String[] args) {
+        HashSet<String> set;
+        set = new HashSet<>();
+        while (!StdIn.isEmpty()) {
+            String key = StdIn.readString();
+            if (!set.contains(key)) {
+                set.add(key);
+                StdOut.print(key + " ");
+            }
+        }
+    }
+}
