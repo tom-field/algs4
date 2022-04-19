2e3c400d2b022463f561d89fe6e57c15498e716d
tom-field <xuhui5018@gmail.com>
2022-04-19 20:42:02 +0800

    拓扑排序用例使用符号有向图

--- src/chapter4/section2/Topological.java
+++ src/chapter4/section2/Topological.java
@@ -24,27 +24,34 @@ public class Topological {
 
     public static void main(String[] args) {
         // 构造书中的图
-        Digraph G = new Digraph(13);
-        G.addEdge(0, 1);
-        G.addEdge(0, 5);
-        G.addEdge(0, 6);
-        G.addEdge(2,0);
-        G.addEdge(2, 3);
-        G.addEdge(3,5);
-        G.addEdge(5,4);
-        G.addEdge(6, 4);
-        G.addEdge(6, 9);
-        G.addEdge(7,6);
-        G.addEdge(8,7);
-        G.addEdge(9,10);
-        G.addEdge(9, 11);
-        G.addEdge(9, 12);
-        G.addEdge(11,12);
+        // Digraph G = new Digraph(13);
+        // G.addEdge(0, 1);
+        // G.addEdge(0, 5);
+        // G.addEdge(0, 6);
+        // G.addEdge(2,0);
+        // G.addEdge(2, 3);
+        // G.addEdge(3,5);
+        // G.addEdge(5,4);
+        // G.addEdge(6, 4);
+        // G.addEdge(6, 9);
+        // G.addEdge(7,6);
+        // G.addEdge(8,7);
+        // G.addEdge(9,10);
+        // G.addEdge(9, 11);
+        // G.addEdge(9, 12);
+        // G.addEdge(11,12);
+        // Topological top = new Topological(G);
+        // for (int v : top.order) {
+        //     StdOut.print(v + " ");
+        // }
 
-        Topological top = new Topological(G);
-
-        for (int v : top.order) {
-            StdOut.print(v + " ");
-        }
+        // 新增SymbolDigraph处理有向图
+        String filename = args[0];
+        String separator = args[1];
+        SymbolDigraph sg = new SymbolDigraph(filename, separator);
+        Topological top = new Topological(sg.G());
+         for (int v : top.order) {
+             StdOut.println(sg.name(v));
+         }
     }
 }
