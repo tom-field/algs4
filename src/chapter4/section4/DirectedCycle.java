6f5fe4f762868f5e5567174d92dd356c4a1d895e
tom-field <xuhui5018@gmail.com>
2022-05-02 12:40:21 +0800

    关键路径算法,修复检测有向图环的bug

--- src/chapter4/section4/DirectedCycle.java
+++ src/chapter4/section4/DirectedCycle.java
@@ -4,13 +4,13 @@ import edu.princeton.cs.algs4.Stack;
 
 public class DirectedCycle {
     private boolean[] marked;
-    private  int[] edgeTo;
-    private Stack<Integer> cycle;   // 有向环中的所有顶点(如果存在)
+    private DirectedEdge[] edgeTo;
+    private Stack<DirectedEdge> cycle;   // 有向环中的所有顶点(如果存在)
     private boolean[] onStack;      // 递归调用的栈上的所有顶点
 
     public DirectedCycle(EdgeWeightedDigraph G) {
         onStack = new boolean[G.V()];
-        edgeTo = new int[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
         marked = new boolean[G.V()];
 
         for (int v = 0; v < G.V(); v++) {
@@ -23,22 +23,28 @@ public class DirectedCycle {
     private void dfs(EdgeWeightedDigraph G, int v) {
         onStack[v] = true;
         marked[v] = true;
+
         for (DirectedEdge e : G.adj(v)) {
             int w = e.to();
             if (this.hasCycle()) {
                 return;
             } else if (!marked[w]) {
-                edgeTo[w] = v;
+                edgeTo[w] = e;
                 dfs(G, w);
             } else if (onStack[w]) {
                 cycle = new Stack<>();
-                for (int x = v; x != w; x = edgeTo[x]) {
-                    cycle.push(x);
+                DirectedEdge edgeInCycle = e;
+
+                while (edgeInCycle.from() != w) {
+                    cycle.push(edgeInCycle);
+                    edgeInCycle = edgeTo[edgeInCycle.from()];
                 }
-                cycle.push(w);
-                cycle.push(v);
+
+                cycle.push(edgeInCycle);
+                return;
             }
         }
+
         onStack[v] = false;
     }
 
@@ -46,7 +52,7 @@ public class DirectedCycle {
         return cycle != null;
     }
 
-    public Iterable<Integer> cycle() {
+    public Iterable<DirectedEdge> cycle() {
         return cycle;
     }
 }
